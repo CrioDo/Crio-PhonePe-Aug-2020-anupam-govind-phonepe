fc703e949aca76aaec5572e251f9e2a4e4ae4bc7
Jenkins <jenkins@common-infra-jenkins-1.asia-south1-c.c.crio-vpn-common.internal>
2020-09-07 10:15:35 +0530

    Updates

---
+++ precache-manifest.d93ab0ec330c9e023e30083d598e7858.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee0317a6f0614f6408bc2da5e74a3de",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/index.html"
+  },
+  {
+    "revision": "9a378904f817c313964f",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "f35876912657587f3880",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "9a378904f817c313964f",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/js/2.9e946520.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/js/2.9e946520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35876912657587f3880",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/js/main.36b45481.chunk.js"
+  },
+  {
+    "revision": "c1605d4d4bdf671054e0",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/js/runtime-main.352f075a.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-PhonePe-Aug-2020-anupam-govind-phonepe/static/media/crio-logo.63be71d0.svg"
+  }
+]);
